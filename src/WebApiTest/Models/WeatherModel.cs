547892426ddfeb716a21d30d4db81292fb3197fa
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2022-03-17 18:03:51 +0300

    feat : add weather model

---
+++ src/WebApiTest/Models/WeatherModel.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApiTest.Models
+{
+    class WeatherModel
+    {
+        [JsonProperty("weather_state_name")]
+        public string WeatherStateName { get; set; }
+
+        [JsonProperty("weather_state_abbr")]
+        public string WeatherAbbr { get; set; }
+
+        [JsonProperty("wind_direction_compass")]
+        public string WindDirectionCompass { get; set; }
+
+        [JsonProperty("created")]
+        public string Created { get; set; }
+
+        [JsonProperty("applicable_date")]
+        public string ApplicableDate { get; set; }
+
+        [JsonProperty("min_temp")]
+        public double MinTemp { get; set; }
+
+        [JsonProperty("max_temp")]
+        public double MaxTemp { get; set; }
+
+        [JsonProperty("wind_speed")]
+        public float WindSpeed { get; set; }
+
+        [JsonProperty("humidity")]
+        public float Humidity { get; set; }
+
+        [JsonProperty("predictability")]
+        public int Predictability { get; set; }
+    }
+}
