6778128ca5aa1fdb0946f268ea5bc8bfc832f688
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2022-03-17 18:06:54 +0300

    feat : add api manager

---
+++ src/WebApiTest/Managers/ApiManager.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiTest.Models;
+
+namespace WebApiTest.Managers
+{
+    internal class ApiManager
+    {
+        private static HttpClient _httpClient = new HttpClient();
+        public static async Task<List<WeatherSearchModel>> SearchLocation(string location)
+        {
+            var result = await _httpClient.GetAsync($"https://www.metaweather.com/api/api/location/search/?query={location}");
+            return JsonConvert.DeserializeObject<List<WeatherSearchModel>>(
+               await result.Content.ReadAsStringAsync());
+        }
+        public static async Task<WeatherResponseModel> GetWeather(int id)
+        {
+            var result = await _httpClient.GetAsync($"https://www.metaweather.com//api/location/{id}/");
+            return JsonConvert.DeserializeObject<WeatherResponseModel>(
+               await result.Content.ReadAsStringAsync());
+        }
+        public static async Task<List<WeatherModel>> GetWeatherPresentDay(int id, string date)
+        {
+            var result = await _httpClient.GetAsync($"https://www.metaweather.com/api/location/{id}/{date}");
+            return JsonConvert.DeserializeObject<List<WeatherModel>>(
+               await result.Content.ReadAsStringAsync());
+        }
+    }
+}
