9c9709b7e0114325970e5291191472d2f90f58f2
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2022-03-17 18:10:14 +0300

    feat : add tests

---
+++ src/WebApiTest/Tests/WebApiTest.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebApiTest.Managers;
+
+namespace WebApiTest
+{
+    public class WebApiTest
+    {
+        private string _location="Minsk";
+        private string _geodata = "53.90255,27.563101";
+        private string _presentDay = DateTime.Now.ToString("yyyy-MM-dd");
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public async Task SearchLocationAndSelectMinTemp()
+        {
+            var searchLocation = await ApiManager.SearchLocation(_location);
+            var weather = await ApiManager.GetWeather(searchLocation.First().WoeId);
+            var asddas = weather.ConsolidatedWeather.Select(x => x.MinTemp);
+
+            Assert.AreEqual(searchLocation.First().Title, _location);
+        }
+        [Test]
+        public async Task CheckGeoData()
+        {
+            var searchLocation = await ApiManager.SearchLocation(_location);
+
+            Assert.AreEqual(searchLocation.First().LattLlong, _geodata);
+        }
+
+        [Test]
+        public async Task WeatherPresentDay()
+        {
+            var presentDay=_presentDay.Replace("-", "/");
+            var searchLocation = await ApiManager.SearchLocation(_location);
+            var weather = await ApiManager.GetWeatherPresentDay(searchLocation.First().WoeId, presentDay);
+
+            Assert.AreEqual(weather.First().ApplicableDate, _presentDay);
+        }
+        [Test]
+        public async Task WeatherPresentDay11()
+        {
+            var requiredDay = DateTime.Now.AddYears(-5).ToString("yyyy-MM-dd").Replace("-", "/");
+            var searchLocation = await ApiManager.SearchLocation(_location);
+            var weather12 = await ApiManager.GetWeatherPresentDay(searchLocation.First().WoeId, requiredDay);
+
+            Assert.IsTrue(weather12.Select(x => x.WeatherStateName).Any());
+        }
+    }
+}
