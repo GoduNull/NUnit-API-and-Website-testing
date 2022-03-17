c08f4064fadb507bbe02790b72c6d4769b10e31c
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2022-03-17 18:04:57 +0300

    feat : add weather search model

---
+++ src/WebApiTest/Models/WeatherSearchModel.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApiTest.Models
+{
+    internal class WeatherSearchModel
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("location_type")]
+        public string LocationType { get; set; }
+
+        [JsonProperty("woeid")]
+        public int WoeId { get; set; }
+
+        [JsonProperty("latt_long")]
+        public string LattLlong { get; set; }
+
+    }
+}
