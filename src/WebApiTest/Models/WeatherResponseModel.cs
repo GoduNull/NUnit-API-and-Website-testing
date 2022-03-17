8a1efedad90333224cc97d05294854a30b9317a2
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2022-03-17 18:05:54 +0300

    feat : add weather response model

---
+++ src/WebApiTest/Models/WeatherResponseModel.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApiTest.Models
+{
+    class WeatherResponseModel
+    {
+        [JsonProperty("title")]
+        public string Location { get; set; }
+
+        [JsonProperty("location_type")]
+        public string LocationType { get; set; }
+
+        [JsonProperty("woeid")]
+        public int WoeId { get; set; }
+
+        [JsonProperty("latt_long")]
+        public string LattLong { get; set; }
+
+        [JsonProperty("timezone")]
+        public string TimeZone { get; set; }
+
+        [JsonProperty("consolidated_weather")]
+        public List<WeatherModel> ConsolidatedWeather { get; set; }
+    }
+}
